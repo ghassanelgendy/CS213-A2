c42cd9c0262bfba69e41b5717dcc3ff19d0fa06b
Rawan Ehab <rawanehab523@gmail.com>
2023-10-31 01:11:00 +0200

    Edited header

--- BigReal/BigReal.h
+++ BigReal/BigReal.h
@@ -1,7 +1,7 @@
 // File: BigReal.h
 // Purpose: Header file for BigReal Class
 // Authors: Ghassan Elgendy - Rawan Ehab - Jana Ramadan
-// IDs:	       20220239     -  20220133  -   20220270
+// IDs:	       20220239     -  20220133  -   20220470
 // Section: S21
 // TA: Rana Abdelkader
 // Date: 30 Oct 2023
--- BigReal/Source.cpp
+++ BigReal/Source.cpp
@@ -1,7 +1,7 @@
 // File: Source.cpp
 // Purpose: This file contains the 'main' function. Program execution begins and ends there.
 // Authors: Ghassan Elgendy - Rawan Ehab - Jana Ramadan
-// IDs:	       20220239     -  20220133  -   20220270
+// IDs:	       20220239     -  20220133  -   20220470
 // Section: S21
 // TA: Rana Abdelkader
 // Date: 30 Oct 2023
