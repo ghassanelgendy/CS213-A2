839bff11ef4f2682b3aea41c41de9018587c37b5
Rawan Ehab <rawanehab523@gmail.com>
2023-10-31 01:09:12 +0200

    Edited constructor

--- BigReal/BigReal.cpp
+++ BigReal/BigReal.cpp
@@ -1,7 +1,7 @@
 // File: BigReal.cpp
 // Purpose: Implementation file for BigReal Class (A user-defined data type that can store unlimited count of characters (numbers) and do operations on them)
 // Authors: Ghassan Elgendy - Rawan Ehab - Jana Ramadan
-// IDs:	       20220239     -  20220133  -   20220270
+// IDs:	       20220239     -  20220133  -   20220470
 // Section: S21
 // TA: Rana Abdelkader
 // Date: 30 Oct 2023
@@ -17,15 +17,20 @@ BigReal::BigReal() : integer(""), fraction("")
 BigReal::BigReal(const string& real)
 {
 	if (regex_match(real, regex("[+-]?\\d*.?\\d+"))) {
-		string temp;
-		temp = real;
 		if ((real.find('.') == string::npos)) isDot = false;
 		if (real[0] == '-' || real[0] == '+') {
-			temp = real.substr(1, real.size() - 1);
-			if(real[0] == '-') sign = '-';
+			if (real[0] == '-') sign = '-';
+			integer = real.substr(1, real.find('.') - 1);
+			if (isDot) fraction = real.substr(integer.size() + 2);
 		}
-		integer = temp.substr(0, temp.find('.'));
-		if (isDot) fraction = temp.substr(integer.size() + 1, temp.size() - 1);
+		else {
+			integer = real.substr(0, real.find('.'));
+			if (isDot) fraction = real.substr(integer.size() + 1);
+		}
+		if (!isDot && sign == '-') { 
+			integer = '-' + integer;
+		}
+
 	}
 	else {
 		integer = '0';
@@ -113,7 +118,7 @@ istream& operator>>(istream& input, BigReal& bigR)
 
 ostream& operator<<(ostream& output, const BigReal& BigR)
 {
-	if (BigR.isDot) output <<  BigR.integer << '.' << BigR.fraction;
+	if (BigR.isDot) output << BigR.sign << BigR.integer << '.' << BigR.fraction;
 	else output << BigR.integer;
 	return output;
 }
