0568201dd778e91804c00cf02be2da64fe781904
janaramadan <146888383+janaramadan@users.noreply.github.com>
2023-11-04 03:22:10 +0200

    Added Comments Problem 6

--- Sheet[1] Solution/A2_S21_20220470_Problem6.cpp
+++ Sheet[1] Solution/A2_S21_20220470_Problem6.cpp
@@ -13,27 +13,27 @@ static void binaryPrint(int n){
         return;
     }
     else{
-        binaryPrint(n / 2); // bafdal a2sem a2sem a2sem w lazem dool yekono metratebeen keda 3ashan el reminder yegy metsha2leb
-        cout << (n % 2);
+        binaryPrint(n / 2); // back again but this time with n/2
+        cout << (n % 2); // printing the reminder
     }
 }
-static void numbers(string prefix, int k) {
-    if (k == 0) {             // keda keda mesh hazawed haga
+static void numbers(string prefix, int k) { // 00101 k=2 0010100 0010101 0010110 0010111
+    if (k == 0) {             // Nothing to be added
         cout <<prefix << endl;
         return;
     }
-    numbers(prefix + "0", k - 1);  //hayzawed zero  leghayet el base case
-    numbers(prefix + "1", k - 1);
+    numbers(prefix + "0", k - 1);  //adding zeros till the base case is reached
+    numbers(prefix + "1", k - 1);  // adding ones till the base case is reached
 }
 int main() {
-    cout << "type (one) to print one number or (many) to print many numbers: ";
-    string x; cin>>x;
-    if(x =="one"){
+    cout << "Type (1) to use binary print function or type (2) to use numbers function: ";
+    int x; cin>>x;
+    if(x ==1){
         int n;
         cin>>n;
         binaryPrint(n);
     }
-    else if(x == "many"){
+    else if(x == 2){
         string prefix;
         cin>>prefix;
         int k;
