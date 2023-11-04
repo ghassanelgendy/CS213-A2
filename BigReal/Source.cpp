3146d83db23646cf3c8e84ad01757296b5fcc748
Rawan Ehab <rawanehab523@gmail.com>
2023-11-04 02:54:59 +0200

    edited source file

--- BigReal/Source.cpp
+++ BigReal/Source.cpp
@@ -10,5 +10,37 @@
 using namespace std;
 int main()
 {
+    //BigReal x("36547854.3614") , y("514785269.96321");             // SUB: -478,237,415.60181  ,ADD: 551,333,124.3246     //SHA8ALBOTH
+    //BigReal x("-36547854.3614") , y("514785269.96321");           // SUB:-551,333,124.32461    ,ADD: 478,237,415.6018     //SHA8ALBOTH
+	//BigReal x("36547854.3614") , y("-514785269.96321");          //SUB:551,333,124.32461       ,ADD: -478,237,415.60181   //SHA8ALBOTH
+	//BigReal x("-36547854.3614") , y("-514785269.96321");        //SUB:478,237,415.60181        ,ADD:-551,333,124.32461    //SHA8ALBOTH
+	//BigReal x("514785269.96321") , y("36547854.3614");         //SUB:478,237,415.60181         ,ADD:  551,333,124.32461   //SHA8ALBOTH
+	//BigReal x("-514785269.96321"), y("36547854.3614");        //SUB:-551,333,124.32461         ,ADD:-478,237,415.60181    //SHA8ALBOTH
+	//BigReal x("514785269.96321"), y("-36547854.3614");       //SUB:551,333,124.32461           ,ADD:478,237,415.60181     //SHA8ALBOTH
+	BigReal x("-514785269.96321"), y("-36547854.3614");      //SUB:-478,237,415.60181          ,ADD:-551,333,124.32461      //SHA8ALBOTH
 
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+
+     cout << (x + y) << endl;
+	
+    // //TEST CASES
+    //BigReal x("514785269.96321"), y("36547854.3614");
+
+    //// Addition
+    //BigReal add_result = x + y;
+    //cout << "Addition: " << x << " + " << y << " = " << x+y << endl;
+
+    //// Subtraction
+    //cout << "Subtraction: " << x << " - " << y << " = " << x-y << endl;
+
+    //// Greater than
+    //cout << "Greater than: " << x << " > " << y << " = " << (x > y) << endl;
+
+    //// Less than
+    //cout << "Less than: " << x << " < " << y << " = " << (x < y) << endl;
+
+    //return 0;
+
+	
+
+
+}
