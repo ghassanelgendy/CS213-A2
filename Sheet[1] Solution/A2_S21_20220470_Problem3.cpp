1ad8a020be855bffe8b5e16688802b84cbb73199
janaramadan <146888383+janaramadan@users.noreply.github.com>
2023-11-04 03:38:20 +0200

    Updated Problem 3

--- Sheet[1] Solution/A2_S21_20220470_Problem3.cpp
+++ Sheet[1] Solution/A2_S21_20220470_Problem3.cpp
@@ -8,23 +8,26 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-vector <char> result;
 vector <string> split(string target,string delimiter){
+    vector<string> result;
     getline(cin,target);
     cin>> delimiter;
-    for(int i=0;i<target.size();++i){
-        if(target[i]!=delimiter[0])
-            result.push_back(target[i]);// elstring ely ana 3ayza
-        else
-            result.push_back(' '); // spaces bas fel nos 3ashan el kalam maylaza2sh fy ba3d
+    for(int i=0;i<target.size();++i)
+    {
+        if(target[i] != delimiter[0]){
+            result.push_back(string(1, target[i]));   // if they are not equal a string is created with the wanted character only repeated once
+        }
+        else{
+            result.push_back(string(1,' ')); // adding spaces in between
+        }
     }
-    for (int i=0;i<result.size();++i){
-        cout << result[i];   // ben print elvector ba
+    for(int i=0;i<result.size();++i){
+        cout << result[i];
     }
+    return result;
 }
 int main(){
     string target, delimiter;
     split(target,delimiter);
     return 0;
 }
-
