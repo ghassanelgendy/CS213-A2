7188a6fd40e469cc3897c981e327ca42a36e9558
janaramadan <146888383+janaramadan@users.noreply.github.com>
2023-11-04 03:21:23 +0200

    Updated Version Problem 12

--- Sheet[1] Solution/A2_S21_20220470_Problem12.cpp
+++ Sheet[1] Solution/A2_S21_20220470_Problem12.cpp
@@ -10,39 +10,35 @@
 #include <string>
 #include <vector>
 using namespace std;
-vector<int> points; // ely haysheel el point assigned tab heya leh global? so2al helw ma3rfsh egabto
-void Phishing_Scanner(string filename, vector<string> words) {   // elfunction betlef 3ala kelma kelma teshofha fel file wala
-    ifstream file(filename); // efta7 el file awl haga
-    if (file.is_open()) {  // fata7 wala hayt3ebna ma3ah?
-        string line; // string yesheel el lines ely fel file
-        int total = 0;  // han7ot el total points bas ehtart ba tet7at fel function wala fy anhy moseba takhodny
-        for (int i = 0; i < words.size(); i++) {  // hanlef 3ala vector el words kelma kelma
-            int count = 0;  // mabda2eyan el kelma be zero leh fel loop? la2n ana kol kelma 3ayza abd2aha men el awl
-            while (getline(file, line)) {  // tool ma fy lines
-                if (line.find(words[i]) != string::npos) {   // dawary 3al kelma fy kol line law mawgooda
+vector<string> words = {"suspicious", "expire", "important", "install", "bank", "locked", "PIN", "password", "personal","update", "action", "required", "invoice", "hello", "credit", "card", "verification ", "account", "security", "confirm", "link", "online", "transactions", "previous", "activity", "click", "download", "alert", "username", "perform"};
+vector <int> points = {1,2,2,2,3,1,3,3,2,2,1,1,3,1,3,3,2,2,1,2,2,1,3,1,2,3,2,2,3,1};
+void Phishing_Scanner() {
+    ifstream file("Problem12.txt");  // opening the file
+    if (file.is_open()) { // if the file is successfully opened
+        string line;
+        int total = 0;
+        for (int i = 0; i < words.size(); i++) {
+            file.clear(); // clear the end of file flag
+            file.seekg(0, ios::beg); // move the file pointer to the beginning
+            int count = 0;  // To calculate the count of every word
+            while (getline(file, line)) { // reading all the lines in the file
+                if (line.find(words[i]) != string::npos) {  // if the word is present in the file
                     count ++;
                     total += points[i];
                 }
             }
             if (count != 0){
-                cout <<"The word:  "<< words[i] << " was found " << count << " times and its total number of points equals: " << (count*points[i]) << endl;
+                cout <<words[i] << ": was found " << count << " times and its total number of points equals: " << (count*points[i]) << endl;
             }
         }
-        file.close();  // Close the file
         cout << "Total of points: " << total << endl;
     }
-    else {   // el file mafatahsh asasan
+    else {   // The file cannot be opened
         cout << "el file mesh motah" << endl;
     }
 }
 int main()
 {
-    vector<string> words = {"suspicious", "expire", "important", "install", "bank", "locked", "PIN", "password", "personal","update", "action", "required", "invoice", "hello", "credit", "card", "verification ", "account", "security", "confirm", "link", "online", "transactions", "previous", "activity", "click", "download", "alert", "username", "perform"};
-    points = {1,2,2,2,3,1,3,3,2,2,1,1,3,1,3,3,2,2,1,2,2,1,3,1,2,3,2,2,3,1};
-    string filename;
-    cin>>filename;
-    filename = filename + ".txt";
-    Phishing_Scanner(filename, words);
+    Phishing_Scanner();
     return 0;
 }
-
